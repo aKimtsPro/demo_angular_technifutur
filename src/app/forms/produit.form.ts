93c9a54ee213bca431a6ab1c52bf96f8daf761ad
akimtspro <alexandre.kimtsaris@outlook.com>
2022-03-08 12:17:30 +0100

    post demo custom validation

--- src/app/forms/produit.form.ts
+++ src/app/forms/produit.form.ts
@@ -1,12 +1,36 @@
-import { AbstractControlOptions, Validators } from "@angular/forms";
-import { of } from "rxjs";
+import { AbstractControl, ValidationErrors, Validators } from "@angular/forms";
 
 export const PRODUIT_INSERT_FORM = {
-    id: ['', Validators.required],
-    marque: [''],
-    modele: [''],
-    prix: [0, [Validators.required, Validators.min(0)]],
-    stock: [1, Validators.min(0)]
+    'marque': ['', [Validators.required, Validators.minLength(1), Validators.maxLength(30)]],
+    'modele': [undefined,[Validators.minLength(1), Validators.maxLength(30)]],
+    'prix' : [0, [Validators.required, Validators.min(0)]],
+    'stock' : [1, [Validators.min(1), Validators.max(99), pair]],
+    'en_vente' : [true]
+};
+
+function pair(control: AbstractControl) : ValidationErrors | null {
+    if( !control.value || control.value % 2 == 0 )
+        return null;
+
+    return {
+        pair: {
+            message: 'devrait être pair'
+        }
+    }
 }
 
+export function priceDoubleStock(control: AbstractControl) : ValidationErrors | null {
+    const prix = control.value.prix;
+    const stock = control.value.stock;
 
+    if( prix >= 2*stock )
+        return null;
+
+    return {
+        priceDoubleStock: {
+            prix: prix,
+            stock: stock,
+            message: 'le prix devrait être le double du stock'
+        }
+    }
+}
